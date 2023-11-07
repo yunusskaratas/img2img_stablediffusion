0f2f55f7b607925fd4186f3e16224aa66796bf0d
Yunus Karatas <yunus.karatas@getir.com>
2023-11-07 21:33:10 +0300

    initial changes

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, APIRouter
+import uvicorn
+
+app = FastAPI()
+class HelloWorld():
+    def read_hello(self):
+        return {"data": "Hello World"}
+router = APIRouter()
+router.add_api_route('/api/v2/hello-world', 
+endpoint = HelloWorld().read_hello, methods=["GET"])
+app.include_router(router)
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
