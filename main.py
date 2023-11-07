2a1c913099a07204d0333a97e47b7a6ab9057f34
Yunus Karatas <yunus.karatas@getir.com>
2023-11-07 22:01:16 +0300

    adding new folder and change main

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI, APIRouter
 import uvicorn
 
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+
 app = FastAPI()
-class HelloWorld():
-    def read_hello(self):
-        return {"data": "Hello World"}
-router = APIRouter()
-router.add_api_route('/api/v2/hello-world', 
-endpoint = HelloWorld().read_hello, methods=["GET"])
-app.include_router(router)
+
+@app.post("/upload/")
+async def upload_image(image: UploadFile):
+    # Ensure the uploaded file is a PNG image
+    if image.content_type != "image/png":
+        return {"error": "Only PNG images are allowed."}
+    
+    # Save the uploaded PNG image to a temporary directory
+    save_path = Path("uploads") / image.filename
+    with save_path.open("wb") as f:
+        f.write(image.file.read())
+    
+    return {"message": "Image uploaded successfully."}
+
 
 if __name__ == "__main__":
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
